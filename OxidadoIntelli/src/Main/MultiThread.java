741e4c5d5e2ccb1f9e484a123c9ebb9841a21710
Mr_Metalero <danymena7@gmail.com>
2024-09-12 13:00:32 +0200

    subido heheh

---
+++ OxidadoIntelli/src/Main/MultiThread.java
@@ -0,0 +1,21 @@
+public class MultiThread extends Thread {
+
+    @Override
+    public void run(){
+        for (int i = 0; i < 30; i++) {
+            System.out.println(i);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+    }
+
+
+
+
+}
