1ce66d736acf64f262f8aa8db4efff16a5a8ae08
Mr_Metalero <danymena7@gmail.com>
2024-09-23 12:32:44 +0200

    guardado en la carpeta

--- BaseDatosCiclistas/ConsultasCiclistas/Consulta25.sql
+++ BaseDatosCiclistas/ConsultasCiclistas/Consulta25.sql
@@ -4,3 +4,10 @@ INNER JOIN puerto USING (netapa)
 SELECT netapa FROM puerto 
 WHERE puerto.altura > ANY (SELECT netapa, nompuerto FROM etapa 
 INNER JOIN puerto USING (netapa) WHERE altura > 700)
+
+
+
+
+
+
+
