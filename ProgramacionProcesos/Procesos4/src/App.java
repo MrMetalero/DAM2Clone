0be7c5fff53e5fe5b57ddc22f996b6051ac60704
Mr_Metalero <danymena7@gmail.com>
2024-09-26 12:15:44 +0200

    PSP ejercicios 26

--- ProgramacionProcesos/Procesos4/src/App.java
+++ ProgramacionProcesos/Procesos4/src/App.java
@@ -12,19 +12,26 @@ public class App {
         Boolean encendido = true;
       
         try {
-            ProcessBuilder pb = new ProcessBuilder("/bin/bash");
+            ProcessBuilder pb = new ProcessBuilder("bash","-c","ls","-l");
             Process proceso1 = pb.start();
-            HiloComprobante hiloNuevo = new HiloComprobante();
+            InputStream is = proceso1.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+            BufferedReader bf = new BufferedReader(isr);  
 
-            hiloNuevo.start();
+            // hiloNuevo.start();
 
             do {
-                hiloNuevo.lecturaProceso(proceso1);
+                String txtRead = bf.readLine(); 
+    
+                if ( txtRead != null) {
+                    System.out.println(txtRead);
+                }
+        
             } while (encendido);
             
           
-    
-    
+            
+           
 
 
             
