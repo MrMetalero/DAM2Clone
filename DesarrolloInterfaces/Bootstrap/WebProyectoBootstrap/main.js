71495bd0355873336b0ae0f85ac79f8327d62102
Mr_Metalero <danymena7@gmail.com>
2024-12-11 21:15:32 +0100

    cositas de estilo web

--- DesarrolloInterfaces/Bootstrap/WebProyectoBootstrap/main.js
+++ DesarrolloInterfaces/Bootstrap/WebProyectoBootstrap/main.js
@@ -11,14 +11,14 @@ function fadeIn(elementos, delay = 300) {
  
     elementos.forEach((elemento, index) => {
         setTimeout(() => {
-            elemento.style.opacity = "0.8";
+            elemento.style.opacity = "0.9";
             
             elemento.style.transform = "translateY(0)";
 
 
 
           // Selecciona todos los h3 y p dentro del árbol del elemento (incluso si hay un div intermedio)
-          const headersAndParagraphs = elemento.querySelectorAll('div h3, div p');
+          const headersAndParagraphs = elemento.querySelectorAll('div h3, div p, div a');
 
           // Aplica la opacidad a 1 para cada h3 y p encontrado
           headersAndParagraphs.forEach((innerElement) => {
