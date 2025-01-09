d75c1aa291127109749f070a6501736375b4651b
Mr_Metalero <danymena7@gmail.com>
2025-01-09 14:01:59 +0100

    tengo que reventar este proyecto porque da error

---
+++ DesarrolloInterfaces/Angular/PrimeraApp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello World</h1>',
+  styleUrls: ['./app.component.css'],
+ 
+})
+export class AppComponent {
+  title = 'homes';
+}
