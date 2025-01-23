7b9bc3fc5c517914868f472075db5442fe54cb07
Mr_Metalero <danymena7@gmail.com>
2025-01-24 00:52:55 +0100

    bases estudiando

---
+++ AccesoDatos/EjercicioPruebaEvaluacion2020/DiseñoLogico.sql
@@ -0,0 +1,49 @@
+
+
+
+productos(nombre_producto:varchar,precio:double,fk_nombre_cliente:varchar){
+VNN: nombre_producto
+VNN: precio
+Caj: fk_nombre_cliente clientes(nombre_cliente)
+}
+
+cliente(nombre_cliente:varchar,telefono:double){
+VNN: nombre_cliente
+VNN: telefono
+}
+
+formulas(fk_nombre_producto:varchar,fecha_comercializacion:date){
+VNN: fk_nombre_producto
+VNN: fecha_comercializacion
+Caj: fk_nombre_producto productos(nombre_producto)
+}
+
+componentes(fk_nombre_producto:varchar){
+VNN fk_nombre_producto
+Caj: fk_nombre_producto productos(nombre_producto)
+}
+
+piezas(nombre_pieza:varchar,peso:double,fk_nombre_componente:varchar){
+VNN: nombre_pieza
+VNN: peso
+VNN: fk_nombre_componente
+Caj: fk_nombre_componente componentes(fk_nombre_producto)
+}
+
+
+
+RI1: El precio de un producto nunca podrá ser inferior a 3€
+
+RI2: No puede existir un producto en la tabla Componentes y
+     estar también en la tabla Fórmulas y viceversa
+
+RI3: No pueden haber mas de 4 registros en piezas por cada nombre_componente
+
+RI4: Al insertar un registro en la tabla Productos, se debe de insertar un componente
+     o una formula con su id
+
+RI5: Si se inserta un registro en componentes, debe haber al menos 1 registro con su id 
+     en componentes_contienen
+
+
+
---
+++ AccesoDatos/EjercicioPruebaEvaluacion2020/Tablas.sql
@@ -0,0 +1,59 @@
+-- Active: 1727983525436@@127.0.0.1@5432@compoformula
+
+BEGIN TRANSACTION;
+
+--DEFERRABLE--
+CREATE TABLE productos(
+    nombre_producto VARCHAR,
+    precio FLOAT NOT NULL,
+    CONSTRAINT pk_nombre_producto PRIMARY KEY (nombre_producto) 
+); 
+
+
+CREATE TABLE clientes(
+    nombre_cliente VARCHAR PRIMARY KEY,
+    telefono INTEGER NOT NULL
+); 
+
+CREATE TABLE interesado(
+    fk_nombre_cliente VARCHAR,
+    fk_nombre_producto VARCHAR,
+    CONSTRAINT pk_nombre_cliente_nombre_producto PRIMARY KEY (fk_nombre_cliente,fk_nombre_producto),
+    CONSTRAINT fk_interesado_producto FOREIGN KEY (fk_nombre_producto) REFERENCES productos(nombre_producto),
+    CONSTRAINT fk_interesado_cliente FOREIGN KEY (fk_nombre_cliente) REFERENCES clientes(nombre_cliente)
+); 
+
+CREATE TABLE formulas(
+    fk_nombre_producto VARCHAR PRIMARY KEY,
+    fecha_comercializacion DATE NOT NULL,
+    CONSTRAINT fk_formulas_producto FOREIGN KEY (fk_nombre_producto) REFERENCES productos(nombre_producto)
+); 
+
+--DEFERRABLE--
+CREATE TABLE componentes(
+    fk_nombre_producto VARCHAR,
+    CONSTRAINT fk_componentes_producto FOREIGN KEY (fk_nombre_producto) REFERENCES productos(nombre_producto),
+    CONSTRAINT pk_fk_nombre_producto PRIMARY KEY (fk_nombre_producto) 
+); 
+
+--DEFERRABLE--
+CREATE TABLE piezas(
+    nombre_pieza VARCHAR,
+    peso FLOAT NOT NULL,
+    CONSTRAINT pk_nombre_pieza PRIMARY KEY (nombre_pieza) 
+); 
+
+CREATE TABLE componentes_contienen(
+    fk_nombre_componente VARCHAR ,
+    fk_nombre_pieza VARCHAR ,
+    CONSTRAINT pk_nombre_componenteo_nombre_pieza PRIMARY KEY (fk_nombre_componente,fk_nombre_pieza),
+    CONSTRAINT fk_componente_contienen FOREIGN KEY (fk_nombre_componente) REFERENCES componentes(fk_nombre_producto)
+); 
+
+
+COMMIT;
+
+ROLLBACK;
+
+
+
